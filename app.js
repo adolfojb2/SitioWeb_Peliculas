b9cc71099f9562679693557e5f10384499a10a63
Adolfo <adolfojb2@gmail.com>
2021-05-15 11:47:18 -0500

    Mi primer commiMi primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//comentarios
+var usuario="adolfo";
+var password = "12345";
+var in_usuario = prompt("Usuario");
+var in_pass = prompt("Contraseña"); 
+if(in_usuario == usuario & in_pass == password){
+    alert("Bienvenido");
+    alert("Hola "+ usuario)
+
+}else{
+    alert("Datos incorrectos!!" + valor);
+}
+
+let numero = null;
+
+//Undefined : Variable no definida
